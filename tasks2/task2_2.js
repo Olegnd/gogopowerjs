8c297ad4ccd4b53351c839114c90024a56096511
Olegnd <olegnd63@gmail.com>
2016-10-18 22:54:11 +0300

    update task2_2.js

--- tasks2/task2_2.js
+++ tasks2/task2_2.js
@@ -1,21 +1,15 @@
 'use strict';
-var b;
-function a(n) {
-    a.value = ++n;
-      function b() {
-        return b.call++;
-      };
-      b.call = 1;
-      
-console.log('value = ',value);
-console.log('call  = ',call);    
-      return b;
-    }
-b = a(10);
-
-console.log(b());
-console.log(b());
-console.log(b());
-console.log(b());
+var increment;
+function a() {
+  var c = 1;
+  return function (n) {
+    return { call: c++, value: n + 1 }
+  }
+}
+increment = a();
 
 
+console.log(increment(1));
+console.log(increment(5));
+console.log(increment(10));
+console.log(increment(20));
