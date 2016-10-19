fea69e40e3226fb4061e4318da3ac3b3bcfe11d5
Olegnd <olegnd63@gmail.com>
2016-10-20 01:32:59 +0300

    update task3

---
+++ tasks3/index.js
@@ -0,0 +1,34 @@
+
+(function () {
+
+	function getRandom(min, max) {
+		return Math.floor(min + (max-min) * Math.random());
+	}
+
+	function setSlide(number) {
+		var activeSlide = $('.slider__list').children(':nth-child(' + number + ')');
+		var list = $('.slider__list');
+		list.css('transform',  'translateX('+ (-100 * number ) + '%)');
+		list.children().removeClass('active');
+		activeSlide.addClass('active');
+		console.log('SLIDE HAS BEEN CHANGED');
+	}
+
+	function getActiveNumber() {
+		return $('.slider__list li.active').index() + 1;
+	}
+
+	function nextSlide() {
+		var activeSlideNumber = getActiveNumber();
+		nextSlideNumber = activeSlideNumber + 1;
+
+		setSlide(nextSlideNumber);
+
+	}
+
+	setInterval(function() {
+		var slide = getRandom(0, 5);
+		nextSlide();
+	}, 4000);
+
+})();
