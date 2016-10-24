c35d91a712379858dfabcf46a17b7c222aae6128
Olegnd <olegnd63@gmail.com>
2016-10-24 22:04:13 +0300

    update task4

--- tasks4/src/js/index.js
+++ tasks4/src/js/index.js
@@ -15,12 +15,13 @@ fetch('https://jsonplaceholder.typicode.com/users')
         arrayKey = Object.keys(data[0]);
 //--form HTML document--
         tr = document.createElement('tr');
-        th = document.createElement('th');
+        //th = document.createElement('th');
         th = '<th>'+ arrayKey[0] + '</th><th>' + arrayKey[1] + '</th><th>' + arrayKey[2] + '</th>';
         thead.innerHTML = '<tr>' + th + '</th>';
         
         for(index; index < data.length; index++) {
             tr = document.createElement('tr');
+            //td = document.createElement('td');
             td = '<td>' + data[index].id + '</td><td>' + data[index].name + '</td><td>' + data[index].username + '</td>';
             tr.innerHTML = '<tr>'+ td +'</tr>';
             tbody.appendChild(tr);
