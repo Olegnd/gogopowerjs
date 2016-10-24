0bb6fe9211726e533949ff974489e7f9498ecae9
Olegnd <olegnd63@gmail.com>
2016-10-24 21:37:20 +0300

    update task4

--- tasks4/src/js/index.js
+++ tasks4/src/js/index.js
@@ -5,13 +5,16 @@ fetch('https://jsonplaceholder.typicode.com/users')
  })
 .then(function(data) {
     var index = 0;
+    var arrayKey = [];
     var table = document.querySelector('table');
     var thead = document.querySelector('thead');
     var tbody = document.querySelector('tbody');
     var tr = document.createElement('tr');    
     var th = document.createElement('th');
-    var td = document.createElement('td');    
-        th = '<th>id</th><th>name</th><th>username</th>';
+    var td = document.createElement('td');
+    arrayKey = Object.keys(data[0]);
+        console.log(arrayKey);
+        th = '<th>'+ arrayKey[0] + '</th><th>' + arrayKey[1] + '</th><th>' + arrayKey[2] + '</th>';
         thead.innerHTML = '<tr>' + th + '</th>';
         
         for(index; index < data.length; index++) {
@@ -20,10 +23,5 @@ fetch('https://jsonplaceholder.typicode.com/users')
             tr.innerHTML = '<tr>'+ td +'</tr>';
             tbody.appendChild(tr);
         }
-    
-    
-    var arrayKey = [];
-        arrayKey = Object.keys(data[0])
-        console.log(arrayKey);
 });
 
