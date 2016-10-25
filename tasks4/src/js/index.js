b613f82a86800771ef66d914a3abe2f6a9f16377
Olegnd <olegnd63@gmail.com>
2016-10-25 11:13:43 +0300

    update tasks4/src/js/index.js

--- tasks4/src/js/index.js
+++ tasks4/src/js/index.js
@@ -5,25 +5,32 @@ fetch('https://jsonplaceholder.typicode.com/users')
     })
     .then(function (data) {
         var index = 0;
+        var i;
         var arrayKey = [];
         var table = document.querySelector('table');
         var thead = document.querySelector('thead');
         var tbody = document.querySelector('tbody');
         var tr;
-        var th;
-        var td;
+        var th = [];
+        var td = [];
         arrayKey = Object.keys(data[0]);
-        //--form HTML document--
+        
+    //--form HTML document--
         tr = document.createElement('tr');
-        //th = document.createElement('th');
-        th = '<th>' + arrayKey[0] + '</th><th>' + arrayKey[1] + '</th><th>' + arrayKey[2] + '</th>';
-        thead.innerHTML = '<tr>' + th + '</tr>';
-
+        for(index; index < arrayKey.length - 4; index++) {
+            th[index] = '<th>' + arrayKey[index] + '</th>';
+        }
+        thead.innerHTML = '<tr>' + th.join('') + '</tr>';
+        
+        index = 0;
         for (index; index < data.length; index++) {
             tr = document.createElement('tr');
             //td = document.createElement('td');
-            td = '<td>' + data[index].id + '</td><td>' + data[index].name + '</td><td>' + data[index].username + '</td>';
-            tr.innerHTML = '<tr>' + td + '</tr>';
+            i = 0;
+            for(i; i < arrayKey.length - 4; i++) {
+                td[i] = '<td>' + data[index][arrayKey[i]] + '</td>';
+            }            
+            tr.innerHTML = '<tr>' + td.join('') + '</tr>';
             tbody.appendChild(tr);
         }
     });
